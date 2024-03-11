fcf4a4db4eeec598b20db5cdf83ee5b916afa9f2
hermanboxcar5 <95950881+hermanboxcar5@users.noreply.github.com>
2024-03-11 10:24:35 -0700

    Update acrv2.js

--- api/acrv2.js
+++ api/acrv2.js
@@ -93,5 +93,5 @@ async function auth(username, password) {
 
 export default exported
 export const config = {
-  schedule: "*/30 * * * *"
+  schedule: "0,5,10,15,20,25,30,35,40,45,50,55 * * * *"
 }
